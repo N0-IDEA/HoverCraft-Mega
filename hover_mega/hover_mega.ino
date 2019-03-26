4568dae3cfd71c17bf26221d624a97abda3a2a5c
Francisco Quintana <francisco.quintana.dosa@gmail.com>
2019-03-26 22:40:44 +0100

    RF Support

--- hover_mega/hover_mega.ino
+++ hover_mega/hover_mega.ino
@@ -1,10 +1,23 @@
 #include<Servo.h>
+#include <SPI.h>
+#include "RF24.h"
 
-Servo ESC[2];
+#define MOTORES 2
 #define MAX_MILLIS_TO_WAIT 1000
 
+Servo ESC[MOTORES];
+
 unsigned long starttime;
-byte msg[4];
+byte msg[3];
+
+//RADIO
+
+bool radioNumber = 0;
+RF24 radio(9,10);
+
+byte addresses[][6] = {"1Node","2Node"}; 
+
+//END RADIO
 
 void blink(int n) {
   for(int i=0;i<n;i++){
@@ -16,22 +29,27 @@ void blink(int n) {
 }
 
 void initializeESC() {
-  for(int i=0;i<2;i++){
-      ESC[i].writeMicroseconds(1000); //1000 = 1ms
+  for(int i=0;i<MOTORES;i++){
+    ESC[i].writeMicroseconds(1000);
   }
+  
   delay(1000);
-  for(int i=0;i<2;i++) {
+  
+  for(int i=0;i<MOTORES;i++) {
     ESC[i].writeMicroseconds(2400);
   }
+  
   delay(2000);
-  for(int i=0;i<2;i++) {
+  
+  for(int i=0;i<MOTORES;i++) {
     ESC[i].writeMicroseconds(1000);
   }
 }
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT); 
-  
+
+  //Motores
   ESC[0].attach(3);
   ESC[1].attach(5);
   
@@ -49,7 +67,10 @@ void setup() {
   
   //Done
   blink(5);
+
+  initRF();
 }
+
 /*
 int getPpm(int bytes) {
   int x = ((float)bytes/(float)255)*800;
@@ -63,25 +84,62 @@ int getPpm(int bytes) {
 void loop() {
   starttime = millis();
   
-  //Max wait
+  /*//Max wait
   while ( (Serial.available()<3) && ((millis() - starttime) < MAX_MILLIS_TO_WAIT) )
   {      
-  }
+  }*/
   
-  if(Serial.available() >= 3)
+  //if(readSerial())
+  if(readRF())
   {
-    Serial.readBytes(msg, 3);
     int id = msg[0];
     int value = 0;
     value |= msg[1];
     value <<= 8;
     value |= msg[2];
+    Serial.print("ID: ");
+    Serial.println(id);
+    Serial.print("Value: ");
+    Serial.println(value);
     
     if(id == 2)
       ESC[id].write(value);
     else {
       ESC[id].writeMicroseconds(value);
     }
-    Serial.write(msg, 3);
   }  
+}
+
+void initRF() {
+  radio.begin();
+
+  radio.enableAckPayload();
+  radio.enableDynamicPayloads();
+  
+  if(radioNumber){
+    radio.openWritingPipe(addresses[1]);
+    radio.openReadingPipe(1,addresses[0]);
+  }else{
+    radio.openWritingPipe(addresses[0]);
+    radio.openReadingPipe(1,addresses[1]);
+  }
+  radio.startListening();
+}
+
+bool readSerial() {
+  if(Serial.available() >= 3){
+    Serial.readBytes(msg, 3);
+    Serial.write(msg,3);
+    return true;
+  }
+  return false;
+}
+
+bool readRF() {
+  byte pipeNo;
+  if (radio.available(&pipeNo)){
+    radio.read( &msg, 3 );
+    return true;
+  }
+  return false;
 }
